62be75d891c69fc4d0b9f0c6346ead6c3b5fc7cd
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-11-18 18:11:43 -0800

    reorg client/server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './routes';
+import Routes from './client/routes';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
