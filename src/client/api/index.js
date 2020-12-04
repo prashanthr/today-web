6bcd5694b823359f521048712885fe22d423e441
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-12-03 18:53:30 -0800

    fix(config): Fix prod config

--- src/client/api/index.js
+++ src/client/api/index.js
@@ -3,6 +3,8 @@ import { get } from 'lodash'
 import { debug } from '../util/debug'
 import config from '../config'
 
+const API_BASE_URL = config.apiBaseUrl
+
 const initialState = {
 	name: 'Stranger',
 	isLoading: true,
@@ -18,7 +20,7 @@ export const buildInitialState = (data) => {
 
 const getDataUrl = (params) => {
 	const { weatherUnit, historyLimit, newsLimit, country, location } = params
-  return `${config.apiBaseUrl}/today?location=${encodeURIComponent(location)}&country=${country}&wod_unit=${weatherUnit}&hod_limit=${historyLimit}&nod_limit=${newsLimit}`
+  return `${API_BASE_URL}/today?location=${encodeURIComponent(location)}&country=${country}&wod_unit=${weatherUnit}&hod_limit=${historyLimit}&nod_limit=${newsLimit}`
 }
 
 export const getData = async (params) => {
--- src/client/util/env.js
+++ src/client/util/env.js
@@ -1,4 +1,3 @@
 import { get } from 'lodash'
 
-export const getNodeEnv = () => get(process,'env.NODE_ENV', 'development').toLowerCase()
-export const isProd = () => getNodeEnv() === 'production'
+export const isProd = () => process.env.NODE_ENV.toLowerCase() === 'production'
