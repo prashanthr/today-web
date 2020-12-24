3b28ae3e2c7a5db720a003ea8bcad25e6301f1c5
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-12-23 18:00:18 -0800

    fix(tracking): Fix GA tracking, add quote heading

--- src/client/components/quote/index.js
+++ src/client/components/quote/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Text from '../text'
+import Heading from '../heading'
 import './index.css'
 
 const Quote = ({ data, colors }) => (
 	<>
 		{data && (
 			<div>
+				<Heading text='Quote of the day' />
 				<blockquote>
 					<p>
 						<Text color={'primary'}>
-							"{data.quote}" - <Text color={'secondary'}>{data.author}</Text>
+							"{data.quote}" - <Text color={'tertiary'}>{data.author}</Text>
 						</Text>
 					</p>
 				</blockquote>
